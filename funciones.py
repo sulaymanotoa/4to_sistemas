15eaaf1e0a432545065e9b35942dcdb9249a8da3
sulaymanotoa <sulay.manotoa7233@utc.edu.ec>
2024-10-30 10:21:53 -0500

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,14 @@
+def sumar(n1,n2):
+    return n1 + n2
+def restar(n1,n2):
+    return n1-n2
+def multiplicar (n1,n2):
+    return n1*n2
+def division(n1,n2):
+    return n1/n2
+n1= int(input("ingrese el nmnero 1:"))
+n2= int(input("ingrese el nmnero 2:"))
+print("la suma es :", sumar (n1,n2) )
+print("la resta es :", restar (n1,n2) )
+print("la multiplicacion es :", multiplicar (n1,n2) )
+print("la division es  :", division (n1,n2) )
