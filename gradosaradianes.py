0fef2d98e9fda5b9b88d782352710f493ad0779f
sulaymanotoa <sulay.manotoa7233@utc.edu.ec>
2024-10-30 10:24:37 -0500

    Create gradosaradianes.py

---
+++ gradosaradianes.py
@@ -0,0 +1,9 @@
+import math
+
+def calcular_raiz_cuadrada(numero):
+    return math.sqrt(numero)
+
+numero = int(input("Ingresa el número: "))
+print("La raíz de", numero, "es:", calcular_raiz_cuadrada(numero))
+print(f"La raíz de {numero} es: {calcular_raiz_cuadrada(numero)}")
+print(f"La raíz de {numero} es: {calcular_raiz_cuadrada(numero)}")
